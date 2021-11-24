060ad455faa81b0176972970a60610b7719bbd38
1001pepi <tanankemr@gmail.com>
2021-11-24 05:30:32 +0100

    push commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask import render_template
+
+import pickle
+
+app = Flask(__name__)
+
+#chargement des modèles
+file1 = open("static/models/count_vectorizer_model.sav", 'rb')
+vectorizer = pickle.load(file1)
+
+file2 = open("static/models/logistic_regression_model.sav", 'rb')
+lr = pickle.load(file2)
+
+@app.route("/api/spamdetector", methods=["POST", "GET"])
+def detector():
+
+    if request.method == 'POST':
+        mail = request.form['mail']
+  
+        mail_2 = vectorizer.transform([mail]).toarray()
+        p = lr.predict_proba(mail_2.reshape(1, -1))[0]
+
+        message = "Ce mail est un spam à " + str(p[0]) + " %."
+
+        return render_template("home.html", message=message, mail=mail)
+
+    else:
+        return render_template("home.html")
+    
+
+if __name__ == "__main__":
+    app.run(port=8001, debug=True, host="127.0.0.1")
